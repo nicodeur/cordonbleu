528d65c615c6f0b5c135c306a107de9d84eb9262
Nicolas Deblock <nico.deblock@gmail.com>
2017-09-29 11:45:07 +0200

    modification for docker

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ cd ${0%/*}
 pkill -f cordonbleu-main || true
 
 echo "Starting up..."
-nohup java -jar cordonbleu-main-0.0.1-SNAPSHOT.jar server configuration.json >> cordonbleu.log &
+nohup java -jar cordonbleu-main-0.0.1-SNAPSHOT.jar server conf/configuration.json >> log/cordonbleu.log &
 echo "Started!"
